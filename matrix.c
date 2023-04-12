b247a7444154e49353944fd35ab98fd39f91a181
Chanhee Lee <gogumae159@gmail.com>
2023-04-12 20:46:25 +0900

    행렬 값 입력 수정 및 출력 확인

--- matrix.c
+++ matrix.c
@@ -13,7 +13,7 @@ int row, col;
 
 void main() {
 
-    printf("[----- [이찬희] [2019068057] -----]");
+    printf("[----- [이찬희] [2019068057] -----]\n");
 
     //행렬의 행과 열의 수를 입력받음
     printf("행렬의 행 수를 입력하십시오. > ");
@@ -35,18 +35,31 @@ void main() {
     }
     
     //반복문을 통해 행렬에 값을 입력 받음
-    //iar = Input matrixA Row, iac Input matrixA Column
+    //i[a/b][r/c] = Input [matrixA/matrixB] [Row/Column]
     for(int iar = 0; iar < row; iar++) {  //매 행마다
-    printf("A행렬의 %d행에 값을 입력합니다. %d개 숫자 입력 가능\n> ", iar + 1, col);
         for(int iac = 0; iac < col; iac++) {  //매 열마다
-            scanf("%d", matrixA[iar][iac]);
+            printf("A행렬 %d행 %d열의 값을 입력하십시오.\n", iar + 1, iac + 1);
+            scanf("%d", &matrixA[iar][iac]);
         }
     }
 
     for(int ibr = 0; ibr < row; ibr++) {  //매 행마다
-    printf("B행렬의 %d행에 값을 입력합니다. %d개 숫자 입력 가능\n> ", ibr + 1, col);
         for(int ibc = 0; ibc < col; ibc++) {  //매 열마다
-            scanf("%d", matrixB[ibr][ibc]);
+            printf("B행렬 %d행 %d열의 값을 입력하십시오.\n", ibr + 1, ibc + 1);
+            scanf("%d", &matrixB[ibr][ibc]);
         }
     }
+
+    print_matrix(matrixA);
+    print_matrix(matrixB);
+}
+
+void print_matrix(int **arr) {
+    for(int or = 0; or < row; or++) {
+        for(int oc = 0; oc <col; oc++) {
+            printf("%d ", arr[or][oc]);
+        }
+        printf("\n");
+    }
+    printf("\n\n");
 }
